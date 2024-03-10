ca21b8a521bebbc980a5d81f45f659d39ccde2f2
corbfon <corbin.fonville@gmail.com>
2024-03-09 20:42:18 -0600

    add mass

--- objects/PhysicsObject.py
+++ objects/PhysicsObject.py
@@ -4,18 +4,20 @@ class PhysicsObject:
     pos: Vector2
     vel: Vector2
     acc: Vector2
+    mass: float
     gravity_impact: float
 
-    def __init__(self, pos: Vector2, vel: Vector2 = Vector2(0, 0), acc: Vector2 = Vector2(0, 0), gravity_impact = 1.0) -> None:
+    def __init__(self, pos: Vector2, vel: Vector2 = Vector2(0, 0), acc: Vector2 = Vector2(0, 0), mass=1, gravity_impact = 1.0) -> None:
         self.pos = pos
         self.vel = vel
         self.acc = acc
+        self.mass = mass
         self.gravity_impact = gravity_impact
     
-    def update(self, dt: float, accels: list[Vector2]):
+    def update(self, dt: float, forces: list[Vector2] = []):
         acc_sum = Vector2()
-        for acc in accels:
-            acc_sum += acc
+        for force in forces:
+            acc_sum += force / self.mass
         d_pos = self.vel * dt
         d_vel = self.acc * dt
         d_acc = acc_sum - self.acc
