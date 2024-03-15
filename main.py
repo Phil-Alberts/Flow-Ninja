11ee39758d0d63a3d6400459de7d7cbd28d10347
corbfon <corbin.fonville@gmail.com>
2024-03-15 08:41:13 -0500

    clean out main.py

--- main.py
+++ main.py
@@ -18,10 +18,6 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(character)
 all_sprites.add(floor)
 
-x, y = CHARACTER_INIT_POS
-jump_speed = 0
-gravity = 1
-
 running = True
 while running:
 
@@ -34,25 +30,8 @@ while running:
             key_events.append(event)
     screen.fill((255, 255, 255))
 
-    #Jumpting test
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        x+=5
-    if keys[pygame.K_a]:
-        x-=5
-    if keys[pygame.K_w] and y == GROUND_Y:
-        jump_speed = -15
-    y += jump_speed
-    jump_speed += gravity
-    if y > GROUND_Y:
-        y = GROUND_Y
-        jump_speed = 0
-
     character.update(1/60, pygame.key.get_pressed(), key_events)
     all_sprites.draw(screen)
-    # screen.blit(character.surf, (x, y))
-    # screen.blit(floor.surf, floor.rect)
-    # pygame.draw.rect(screen, (50, 50, 50), (0, GROUND_Y + character.surf.get_height() - 7, SCREEN_WIDTH, SCREEN_HEIGHT))
 
     pygame.display.flip()
     clock.tick(FPS)
