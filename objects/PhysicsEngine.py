16b064fdca56c9545b6408524275043065b56691
corbfon <corbin.fonville@gmail.com>
2024-03-16 18:21:03 -0500

    WIP objects -> sprites

--- objects/PhysicsEngine.py
+++ objects/PhysicsEngine.py
@@ -1,10 +1,10 @@
 from pygame.math import Vector2
-from objects.Physics import Physics
+from pygame.sprite import Group
 from objects.SpriteWithPhysics import SpriteWithPhysics
 import pygame
 
 class PhysicsEngine:
-  objects: list[SpriteWithPhysics] = []
+  objects: Group = Group()
   default_gravity: Vector2
 
   def __init__(self, gravity_const: float = 100.0) -> None:
@@ -12,14 +12,14 @@ class PhysicsEngine:
     pass
 
   def register_object(self, object: SpriteWithPhysics):
-    self.objects.append(object)
+    self.objects.add(object)
   
   def update_entities(self, dt: float, keys_pressed: pygame.key.ScancodeWrapper, key_events):
     # loop through objects and develop expected position
     #  record any events that should happen
     # loop through objects and publish events
     events = []
-    for obj in self.objects:
+    for obj in self.objects.sprites():
       if obj.physics is None:
         continue
 
